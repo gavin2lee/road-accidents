8123c1cc2636d51952df41c1d6130fb758c538b1
gavin <gavin2lee@163.com>
2016-05-13 13:53:11 +0800

    init

---
+++ src/main/java/com/gl/roadaccidents/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.gl.roadaccidents;
+
+/**
+ * Created by gavin on 16-5-13.
+ */
+public class HelloWorld {
+    public static void main(String...args){
+        System.out.println("Hello World");
+    }
+
+}
